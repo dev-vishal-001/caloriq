fc5d18ebfb087293525635660b841b0abe1bb78f
Vishal Durga DR <dev.vishaldurga@gmail.com>
2025-07-02 19:23:42 +0530

    Update register.ts

--- frontend/app/api/register.ts
+++ frontend/app/api/register.ts
@@ -28,7 +28,7 @@ export interface SignInResponse {
 }
 
 export async function registerRequest(body: RegisterRequest): Promise<SignInResponse> {
-  const res = await fetch("http://localhost:5050/api/auth/register", {
+  const res = await fetch("https://caloriq-backend.onrender.com/api/auth/register", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
